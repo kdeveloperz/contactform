d8989c43b3ec9ecb4380ef946c4cff9daae4f3a6
Kace Developerz <kdeveloperzagency@gmail.com>
2025-04-07 13:40:57 +0100

    seven aprill commit

--- config/config.php
+++ config/config.php
@@ -1,4 +1,6 @@
 <?php
 return [
-    'admin_email' => env('ADMIN_EMAIL', '')
+    'admin_email' => env('ADMIN_EMAIL', ''),
+
+    'contact_url' => env('KACE_CONTACT_URL', 'kace-contact-form'),
 ];
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Kacedeveloperz\Contactform\Http\Controllers\ContactFormController;
 
 Route::middleware(['guest', 'web'])->group(function () {
-    Route::get('/contact', [ContactFormController::class, 'create'])->name('kace.contact.form');
+    
+    Route::get(config('kace_contactform.contact_url'), [ContactFormController::class, 'create'])->name('kace.contact.form');
     Route::post('/submit/message', [ContactFormController::class, 'store'])->name('kace.contact');
 });
